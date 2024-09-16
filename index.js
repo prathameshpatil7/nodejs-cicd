1daf65be2218a0020bdbb2d8664035e0c8fe2467
prathameshpatil7 <patil444411@gmail.com>
2024-09-16 20:10:49 +0530

    added /books

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/books", (req, res) => res.send("Books on Vercel"));
 
 app.listen(3003, () => console.log("Server ready on port 3003."));
 
